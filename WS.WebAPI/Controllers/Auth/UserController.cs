f10a8bca42ca9c4603776802319ecea5932b8f82
DanhHieuuuuu <hieunguyendanh91@gmail.com>
2024-12-08 00:33:55 +0700

    .

--- WS.WebAPI/Controllers/Auth/UserController.cs
+++ WS.WebAPI/Controllers/Auth/UserController.cs
@@ -16,7 +16,7 @@ namespace WS.WebAPI.Controllers.Auth
         [HttpGet("/test")]
         public IActionResult Get()
         {
-            return Ok("hello");
+            return Ok("hello word");
         }
     }
 }
